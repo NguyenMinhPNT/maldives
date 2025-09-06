85f1831fc9f6a93143a34a79da22be77b283879e
NguyenMinhPNT <nminh212@gmail.com>
2025-09-06 10:36:02 +0700

    clean theme

--- lib/main.dart
+++ lib/main.dart
@@ -19,31 +19,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key}); // <-- Added missing constructor
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+      body: const Center(
+        child: Text('Hello, World!', style: TextStyle(fontSize: 50)),
       ),
-      body: const Center(child: Text('Hello, World!')),
     );
   }
 }
